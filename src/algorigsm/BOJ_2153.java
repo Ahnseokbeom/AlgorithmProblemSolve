e44ed47dac5a95c0971bbd5cecf90b0e792c0e4d
seobkeom <ajm153123@naver.com>
2022-05-15 21:02:02 +0900

    BOJ_2153 commit

--- src/algorigsm/BOJ_2153.java
+++ src/algorigsm/BOJ_2153.java
@@ -6,16 +6,16 @@ import java.io.InputStreamReader;
 
 public class BOJ_2153 {
 	public static void main(String[] args) throws IOException{
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String word = br.readLine();
 		int sum = 0;
 		for(int i = 0;i<word.length();i++) {
 			char c = word.charAt(i);
 			if(c >= 'A' && c <= 'Z') {
-                sum += (int)c - 38;
+                sum += c - 38;
             } else if (c >= 'a' && c <= 'z') {
-                sum += (int)c - 96;
+                sum += c - 96;
             }
 		}
 		if(prime(sum)) {
@@ -23,7 +23,7 @@ public class BOJ_2153 {
 		}else {
 			System.out.println("It is not a prime word.");
 		}
-		
+
 }
 	public static boolean prime(int n) {
 		if(n<2){
@@ -32,7 +32,7 @@ public class BOJ_2153 {
 		if(n==2) {
 			return true;
 		}
-		for(int i = 2;i<=Math.sqrt(n);i++) {
+		for(int i = 2;i<=n;i++) {
 			if(n%i==0) {
 				return false;
 			}
