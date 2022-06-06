f4bb689e2f7ea753fc1c2554ad3f441c294d1897
seobkeom <ajm153123@naver.com>
2022-06-06 13:55:37 +0900

    가희와 코딩하는 코딩테스트 문제 1

---
+++ src/CodingTest/BOJ_819_1.java
@@ -0,0 +1,22 @@
+package CodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_819_1 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		if(a-(a*(b*0.01))>=100) {
+			System.out.println(0);
+		}else {
+			System.out.println(1);
+		}
+
+	}
+
+}
