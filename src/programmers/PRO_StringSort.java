b47703bc4fe0f370c962a122fc1fa4d2d47e92e1
seobkeom <ajm153123@naver.com>
2022-06-27 23:14:38 +0900

    PRO commit

--- src/programmers/PRO_StringSort.java
+++ src/programmers/PRO_StringSort.java
@@ -7,12 +7,14 @@ public class PRO_StringSort {
 	class Solution {
 	    public String solution(String s) {
 	        String answer = "";
-	        String[] str = new String[s.length()-1];
+	        String[] str = new String[s.length()];
 	        for(int i = 0;i<s.length();i++){
 	            str[i] = s.charAt(i)+"";
 	        }
-	        Arrays.sort(str);
-
+	        Arrays.sort(str,Collections.reverseOrder());
+	        for(int i = 0;i<str.length;i++) {
+	        	answer+=str[i];
+	        }
 	        return answer;
 	    }
 	}
