72ce0c384fb15ac0e52b0037ca4d04970fc09ee7
seobkeom <ajm153123@naver.com>
2022-06-29 20:04:20 +0900

    PRO 최솟값 최댓값

---
+++ src/programmers/PRO_MaxMin.java
@@ -0,0 +1,23 @@
+package programmers;
+
+public class PRO_MaxMin {
+	public static void main(String[] args) {
+		String s = "-1 -2 -3 -4";
+		String answer = "";
+		String[] str = s.split(" ");
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+		for(int i = 0;i<str.length;i++) {
+			if(Integer.parseInt(str[i])>max) {
+				max = Integer.parseInt(str[i]);
+			}
+			if(Integer.parseInt(str[i])<min) {
+				min = Integer.parseInt(str[i]);
+			}
+		}
+		answer = min+" "+max;
+		System.out.println(answer);
+
+	}
+
+}
