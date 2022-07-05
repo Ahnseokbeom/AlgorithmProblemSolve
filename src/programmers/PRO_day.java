247a73bbc6565448a1b54bf42bf70c1cc9e37ef3
seobkeom <ajm153123@naver.com>
2022-07-05 18:48:30 +0900

    PRO 2016년

---
+++ src/programmers/PRO_day.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class PRO_day {
+	class Solution {
+	    public String solution(int a, int b) {
+
+	        String answer = "";
+	        return answer;
+	    }
+	}
+
+}
