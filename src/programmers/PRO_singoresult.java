677470d584700a9c9bcbf751c2903f432a7414cf
seobkeom <ajm153123@naver.com>
2022-07-07 00:00:17 +0900

    PRO commit

---
+++ src/programmers/PRO_singoresult.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class PRO_singoresult {
+	public static void main(String[] args) {
+		String[] s = {"muzi frodo"};
+		String[] str = s[0].split(" ");
+		System.out.println(str[1]);
+		HashMap<String, Integer> map = new HashMap<>();
+
+	}
+
+}
