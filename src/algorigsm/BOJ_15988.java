87a71e6bb738168379aa62fff2df9df62de8911f
seobkeom <ajm153123@naver.com>
2022-07-28 15:27:31 +0900

    1,2,3으로 숫자 만들기 2개(DP)

--- src/algorigsm/BOJ_15988.java
+++ src/algorigsm/BOJ_15988.java
@@ -4,19 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ_9095 {
+public class BOJ_15988 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
-		int[] dp = new int[11];
+		long[] dp = new long[1000001];
 		dp[1] = 1;
 		dp[2] = 2;
 		dp[3] = 4;
-		while(t-->0) {
+		for(int i = 4;i<=1000000;i++) {
+			dp[i] = (dp[i-1]+dp[i-2]+dp[i-3])%1000000009;
+		}
+		for(int i = 0;i<t;i++) {
 			int n = Integer.parseInt(br.readLine());
-			for(int i = 4;i<=n;i++) {
-				dp[i] = (dp[i-1]+dp[i-2]+dp[i-3]);
-			}
 			System.out.println(dp[n]);
 		}
 	}
