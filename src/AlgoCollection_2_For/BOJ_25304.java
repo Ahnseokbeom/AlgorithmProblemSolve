0b0f8a307d35473851d2acb85351bcc24068ba40
seobkeom <ajm153123@naver.com>
2022-08-10 13:29:12 +0900

    Coding Test 2Problems commit

---
+++ src/AlgoCollection_2_For/BOJ_25304.java
@@ -0,0 +1,24 @@
+package AlgoCollection_2_For;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_25304 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long x = Integer.parseInt(br.readLine());
+		long sum = 0;
+		int n = Integer.parseInt(br.readLine());
+		for(int i = 0;i<n;i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine()," ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sum+=a*b;
+		}
+		if(x==sum) System.out.println("Yes");
+		else System.out.println("No");
+	}
+
+}
