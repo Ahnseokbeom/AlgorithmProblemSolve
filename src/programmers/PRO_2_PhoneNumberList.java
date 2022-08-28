01e5f0414b10b0b46279b04b5d2120b94354883c
seobkeom <ajm153123@naver.com>
2022-08-28 17:43:54 +0900

    프로그래머스 전화번호 목록 commit

--- src/programmers/PRO_2_PhoneNumberList.java
+++ src/programmers/PRO_2_PhoneNumberList.java
@@ -1,23 +1,22 @@
 package programmers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PRO_2_PhoneNumberList {
 	public static void main(String[] args) {
-		String[] str = {"123","456","789"};
-		String[]s = {"12","123","1235","567","88"};
+		String[] phone_book = {"123","456","789"};
+//		String[] phone_book = {"12","123","1235","567","88"};
+//		String[] phone_book = {"12","123","23"};
 		ArrayList<String> arr = new ArrayList<>();
-		for(int i = 0;i<str.length;i++) {
-			arr.add(str[i]);
-		}
-		boolean check = true;
-		for(int i = 0;i<arr.size();i++) {
-			for(int j = i+1;j<arr.size();j++) {
-				System.out.println(arr.get(j).contains(str[i]));
-				if(arr.get(j).contains(arr.get(i))) check = false;
+		Arrays.sort(phone_book);
+		boolean answer = true;
+		for(int i = 0;i<phone_book.length-1;i++) {
+			if(phone_book[i+1].startsWith(phone_book[i])) {
+				answer = false;
 			}
 		}
-		System.out.println(check);
+		System.out.println(answer);
 	}
 
 }
