9254e9324b8a0d4e6eb585431c4b7f11b7cf99f8
seobkeom <ajm153123@naver.com>
2022-10-16 00:35:47 +0900

    BOJ_13900 commit

--- src/algorigsm/BOJ_13900.java
+++ src/algorigsm/BOJ_13900.java
@@ -3,21 +3,26 @@ package algorigsm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class BOJ_13900 {
 	public static void main(String[] args)throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		String[] list = br.readLine().split(" ");
-		int sum = 0;
-		for(int i = 0;i<n;i++) {
-			int tempSum = 0;
-			for(int j = i+1;j<n;j++) {
-				tempSum += Integer.parseInt(list[j]);
-			}
-			sum += Integer.parseInt(list[i])*tempSum;
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		Long[] num = new Long[n+1];
+		Long[] sum = new Long[n+1];
+		num[0] = 0L;
+		sum[0] = 0L;
+		for(int i = 1;i<=n;i++) {
+			num[i] = Long.parseLong(st.nextToken());
+			sum[i] = num[i]+sum[i-1];
 		}
-		System.out.println(sum);
+		Long answer = 0L;
+		for(int i = 2;i<=n;i++) {
+			answer+=num[i]*sum[i-1];
+		}
+		System.out.println(answer);
 	}
 
 }
