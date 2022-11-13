addb3491dbe5230059559fc65c1d2ecce9438fb7
seobkeom <ajm153123@naver.com>
2022-11-13 18:31:18 +0900

    Algo Practice

---
+++ src/Algo/Node.java
@@ -0,0 +1,26 @@
+package Algo;
+
+public class Node {
+	static class node{
+		int value;
+		node next;
+		public node(int value, node next) {
+			this.value=value;
+			this.next=next;
+		}
+		public void printAll() {
+			if(value==0) return;
+			System.out.printf("%d ",value);
+			value--;
+			printAll();
+		}
+	}
+	public static void main(String[] args) {
+		node root = null;
+		for(int i = 1;i<=10;i++) {
+			root = new node(i,root);
+		}
+		root.printAll();
+	}
+
+}
