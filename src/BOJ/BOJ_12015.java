6215f6d196bec61b77a1e65ad8703e034c2ac6ec
seobkeom <ajm153123@naver.com>
2023-02-14 12:05:48 +0900

    BOJ_12015(binarySearch)

--- src/BOJ/BOJ_12015.java
+++ src/BOJ/BOJ_12015.java
@@ -1,31 +1,39 @@
-	package BOJ;
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class BOJ_12015 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a = Integer.parseInt(br.readLine());
-		int[] list = new int[a];
-		int[] dp = new int[a];
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		for(int i = 0;i<a;i++) {
-			list[i] = Integer.parseInt(st.nextToken());
-		}
-		int max = 0;
-		for(int i = 0;i<a;i++) {
-			dp[i] = 1;
-			for(int j = 0;j<i;j++) {
-				if(list[j]<list[i] && dp[i]<=dp[j]) {
-					dp[i] = dp[j]+1;
-				}
-			}
-			max = Math.max(max, dp[i]);
-		}
-		System.out.println(max);
-	}
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(0);
 
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i = 0;i<n;i++) {
+			if(list.get(list.size()-1)<arr[i]) list.add(arr[i]);
+			else {
+				int left = 1;
+				int right = list.size()-1;
+				while(left < right) {
+					int mid = (left+right)/2;
+					if(list.get(mid) < arr[i]) {
+						left = mid+1;
+					}else {
+						right = mid;
+					}
+				}
+				list.set(right, arr[i]);
+			}
+		}
+		System.out.println(list.size()-1);
+	}
 }
