7663c995ba369609c53f702ae69e92afb744322e
seobkeom <ajm153123@naver.com>
2023-04-07 22:53:30 +0900

    프로그래머스 귤 고르기

--- src/programmers/PRO_2_MandarinSelect.java
+++ src/programmers/PRO_2_MandarinSelect.java
@@ -6,7 +6,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PRO_2_MandarinSelect {
-	public int solution(int k, int[] tangerine) {
+	public static void main(String[] args) {
+		int k1 = 6;
+		int[] tang1 = {1,3,2,5,4,5,2,3};
+		System.out.println(solution(k1, tang1));
+
+		int k2 = 4;
+		int[] tang2 = {1,3,2,5,4,5,2,3};
+		System.out.println(solution(k2, tang2));
+
+		int k3 = 2;
+		int[] tang3 = {1,1,1,1,2,2,2,3};
+		System.out.println(solution(k3, tang3));
+	}
+	public static int solution(int k, int[] tangerine) {
 		int answer = 0;
 		Map<Integer, Integer> map = new HashMap<>();
 
@@ -26,6 +39,8 @@ public class PRO_2_MandarinSelect {
 		});
 
 		int i = 0;
+		System.out.println(map);
+		System.out.println(list);
 		while(k>0) {
 			k -= map.get(list.get(i));
 			answer++;
