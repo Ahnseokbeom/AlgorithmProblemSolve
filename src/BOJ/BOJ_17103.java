b90056be5052676ebf490c1df2225c7602d3ee7a
seobkeom <ajm153123@naver.com>
2023-04-29 23:32:32 +0900

    BOJ_17103 recommit

--- src/BOJ/BOJ_17103.java
+++ src/BOJ/BOJ_17103.java
@@ -7,15 +7,14 @@ import java.io.InputStreamReader;
 public class BOJ_17103 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
 
 		boolean[] num = new boolean[1000001];
-		num[0]=num[1] = true;
-		for(int i = 2;i*i<=100000;i++) {
-			if(!num[i]) {
-				for(int j = i+i;j<=100000;j+=i) num[j] = true;
-			}
+		num[0]= num[1] = true;
+		for(int i = 2;i<=Math.sqrt(1000000);i++) {
+			if(num[i])continue;
+			for(int j = i*i;j<=100000;j+=i) num[j] = true;
 		}
+		int t = Integer.parseInt(br.readLine());
 		while(t-- > 0) {
 			int n = Integer.parseInt(br.readLine());
 			int answer = 0;
