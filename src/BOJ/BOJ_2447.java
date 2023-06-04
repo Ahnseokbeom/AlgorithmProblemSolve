e646019884b9837b4420433580e6b0a80b57ded4
seobkeom <ajm153123@naver.com>
2023-06-04 22:08:57 +0900

    BOJ_2447

--- src/BOJ/BOJ_2447.java
+++ src/BOJ/BOJ_2447.java
@@ -5,18 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class BOJ_2447 {
+	static String[][] arr;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		String[][] arr = new String[n][n];
+		arr = new String[n][n];
 		for(int i = 0;i<arr.length;i++) {
 			for(int j = 0;j<arr[0].length;j++) arr[i][j] = " ";
 		}
 		star(arr,0,0,n);
-		for(String[] str : arr) {
-			for(String s : str) System.out.print(s+"");
-			System.out.println(" ");
-		}
+		rec();
 	}
 	public static void star(String[][] arr, int x, int y, int n) {
 		if(n==1) {
@@ -27,6 +25,12 @@ public class BOJ_2447 {
 			for(int j = 0;j<3;j++) {
 				if(!(i==1 && j== 1)) star(arr,x+i*(n/3),y+j*(n/3),n/3);
 			}
+		}
+	}
+	public static void rec() {
+		for(String[] str : arr) {
+			for(String s : str) System.out.print(s+"");
+			System.out.println(" ");
 		}
 	}
 
