bc2437076de23235983022092f96010d785cb9b3
seobkeom <ajm153123@naver.com>
2023-06-07 21:18:40 +0900

    BOJ_13305

---
+++ src/BOJ/BOJ_13305.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+public class BOJ_13305 {
+	public static void main(String[] args) {
+		System.out.println(solution(4, new long[] {2,3,1}, new long[] {5,2,4,1}));
+		System.out.println(solution(4, new long[] {3,3,4}, new long[] {1,1,1,1}));
+	}
+	public static long solution(int n, long[] km, long[] price) {
+		long sum = 0;
+		long min = price[0];
+		for(int i = 0;i<km.length;i++) {
+			if(price[i] < min) min = price[i];
+			sum+=(min*km[i]);
+		}
+		return sum;
+	}
+
+}
