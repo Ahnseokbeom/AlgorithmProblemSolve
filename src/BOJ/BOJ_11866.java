6122d4890720fbbdddabff6cab4597bb8f208440
seobkeom <ajm153123@naver.com>
2023-06-21 12:58:17 +0900

    Code Update

--- src/BOJ/BOJ_11866.java
+++ src/BOJ/BOJ_11866.java
@@ -3,7 +3,6 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -11,30 +10,22 @@ import java.util.StringTokenizer;
 public class BOJ_11866 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Queue<Integer> queue = new LinkedList<Integer>();
+		Queue<Integer> q = new LinkedList<Integer>();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for(int i = 0;i<n;i++) {
-			queue.offer(i+1);
-		}
-		while(!queue.isEmpty()) {
-			for(int i = 0;i<k-1;i++) {
-			int temp = queue.poll();
-			queue.offer(temp);
-			}
-			list.add(queue.poll());
-		}
 		sb.append("<");
-		for(int i = 0;i<list.size();i++) {
-			if(i==list.size()-1) {
-				sb.append(list.get(i)+">");
-			}else {
-				sb.append(list.get(i)+", ");
-			}
+		for(int i = 1;i<=n;i++) {
+			q.offer(i);
 		}
+		while(q.size() > 1) {
+			for(int i = 0;i<k-1;i++) {
+			q.offer(q.poll());
+			}
+			sb.append(q.poll()).append(", ");
+		}
+		sb.append(q.poll()+">");
 		System.out.println(sb.toString());
 	}
 
