3cb3daa48a22d3278c515991dd76914f897d0614
seobkeom <ajm153123@naver.com>
2023-07-08 18:21:02 +0900

    Progrmmers Lv3 Code Update

--- src/programmers/PRO_3_DoublePriorityQueue.java
+++ src/programmers/PRO_3_DoublePriorityQueue.java
@@ -1,34 +1,42 @@
 package programmers;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class PRO_3_DoublePriorityQueue {
-	public int[] solution(String[] operations) {
-        int[] answer = new int[2];
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>(Collections.reverseOrder());
+    public static void main(String[] args) {
+        System.out.println(
+                Arrays.toString(solution(new String[] { "I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1" })));
 
-        String[] temp;
-        for(int i = 0 ; i < operations.length;i++){
-            temp = operations[i].split(" ");
-            if(pq1.size()==0 && temp[0].equals("D"))continue;
-            if(temp[0].equals("I")){
-                pq1.add(Integer.parseInt(temp[1]));
-                pq2.add(Integer.parseInt(temp[1]));
-            }else if(temp[0].equals("D")&&temp[1].equals("1")){
-                int max = pq2.poll();
-                pq1.remove(max);
-            }else if(temp[0].equals("D")&&temp[1].equals("-1")){
-                int min = pq1.poll();
-                pq2.remove(min);
+        System.out.println(Arrays.toString(
+                solution(new String[] { "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" })));
+    }
+
+    public static int[] solution(String[] operations) {
+        int[] answer = new int[2];
+        PriorityQueue<Integer> Minq = new PriorityQueue<>();
+        PriorityQueue<Integer> Maxq = new PriorityQueue<>(Collections.reverseOrder());
+
+        String[] str;
+
+        for (int i = 0; i < operations.length; i++) {
+            str = operations[i].split(" ");
+            if (Minq.size() == 0 && str[0].equals("D"))
+                continue;
+            if (str[0].equals("I")) {
+                Minq.add(Integer.parseInt(str[1]));
+                Maxq.add(Integer.parseInt(str[1]));
+            } else if (str[0].equals("D") && str[1].equals("1")) {
+                Minq.remove(Maxq.poll());
+            } else if (str[0].equals("D") && str[1].equals("-1")) {
+                Maxq.remove(Minq.poll());
             }
         }
-        if(pq1.size()>0){
-            answer[0]=pq2.poll();
-            answer[1]=pq1.poll();
+        if (Minq.size() > 0) {
+            answer[0] = Maxq.poll();
+            answer[1] = Minq.poll();
         }
-
         return answer;
     }
 }
