2a40ef53e3a2cdd5e383c50247d25f6be556ba27
seobkeom <ajm153123@naver.com>
2023-10-06 21:10:16 +0900

    Goorm 파손된 램

---
+++ src/goorm/PRO_파손된램.java
@@ -0,0 +1,27 @@
+package goorm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class PRO_파손된램 {
+	static ArrayList<Integer> arr = new ArrayList<>();
+	static int count = 0;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for(int i = 1;i<=31;i++) arr.add((int)Math.pow(2, i));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		for(int i = 1;i<=n;i++) {
+			if(!arr.contains(Integer.parseInt(st.nextToken()))) {
+				sb.append(i+" ");
+				count++;
+			}
+		}
+		System.out.println(count+"\n"+sb);
+	}
+}
