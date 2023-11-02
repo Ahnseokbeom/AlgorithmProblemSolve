fe70b4c55652e4ca33baa2ad25a03756ee4f8aa0
seobkeom <ajm153123@naver.com>
2023-11-02 15:38:53 +0900

    PRO3 징검다리(DP)

---
+++ src/Softeer/PRO3/PRO3_징검다리.java
@@ -0,0 +1,33 @@
+package Softeer.PRO3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class PRO3_징검다리 {
+	static int[] arr,dp;
+	static int answer;
+	 public static void main(String[] args) throws IOException{
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+      arr = new int[n];
+      dp = new int[n];
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      for(int i = 0;i<n;i++) arr[i] = Integer.parseInt(st.nextToken());
+      answer = 0;
+      System.out.println(solution(n));
+    }
+	public static int solution(int n) {
+		for(int i = 0;i<n;i++){
+	        dp[i] = 1;
+	        for(int j = 0;j<i;j++){
+	          if(arr[i] > arr[j] && dp[i]<=dp[j]){
+	            dp[i] = dp[j]+1;
+	          }
+	        }
+	      }
+      for(int i : dp) answer = Math.max(answer,i);
+      return answer;
+	}
+}
