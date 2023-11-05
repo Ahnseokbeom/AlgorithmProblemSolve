84f64eecd19af06b2b3a3f27d6ef4cb0ed4cb5f6
seobkeom <ajm153123@naver.com>
2023-11-05 17:09:51 +0900

    CodingTest

--- src/CodingTest/PRO2_2023_11_04.java
+++ src/CodingTest/PRO2_2023_11_04.java
@@ -1,33 +1,68 @@
 package CodingTest;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class PRO2_2023_11_04 {
-	static String[] arr1 = {"A","A","B","C","D"};
-	static String[] arr2 = {"B","C","B","D"};
-	public static void main(String[] args) {
-        List<String> listX = new ArrayList<>();
-        List<String> listY = new ArrayList<>();
-        dic(arr1, 0, "", listX);
-        dic(arr2, 0, "", listY);
-        System.out.println(solution(listX, listY));
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int h0 = Integer.parseInt(st.nextToken());
+		int w0 = Integer.parseInt(st.nextToken());
+		int h1 = Integer.parseInt(st.nextToken());
+		int w1 = Integer.parseInt(st.nextToken());
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[13];
+		int x = h0;
+		int y = w0;
+		for(int i = 1;i<13;i++) {
+			if(i<=3) {
+				arr[i] = ItoS(x)+" "+ItoS(y)+" "+ItoS(x)+(2*r)+" "+ItoS(y)+(2*r);
+				y+=(2*r)+w1;
+			}else {
+				String[] str = arr[i-3].split(" ");
+				arr[i] = ItoS(StoI(str[0])+(2*r)+h1)+" "+ItoS(StoI(str[1])+(2*r))+" "+ItoS(StoI(str[2])+(2*r)+h1)+" "+ItoS(StoI(str[3])+(2*r));
+				y+=(2*r)+w1;
+			}
+		}
+		int count = 0;
+		String key = "";
+		Queue<String> q = new LinkedList<>();
+		for(int i = 0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if(a < h0 || b < w0) continue;
+			for(int j = 1;j<13;j++) {
+				String[] str = arr[i].split(" ");
+				if(StoI(str[0])<= a && a <= StoI(str[1]) && StoI(str[2]) <= a && a <= StoI(str[3])) {
+					key+=ItoS(j);
+					break;
+				}
+				if(j==10) {
+					key = key.substring(0,key.length()-1);
+				}
+				if(j==12) {
+					q.add(key);
+					count++;
+					key = "";
+				}
+			}
+			System.out.println(count);
+			while(!q.isEmpty()) System.out.println(q.remove());
+		}
 	}
-   public static void dic(String[] arr, int index, String current, List<String> list) {
-        if (index == arr.length) {
-            list.add(current);
-            return;
-        }
-        dic(arr, index + 1, current + arr[index], list);
-        dic(arr, index + 1, current, list);
-    }
-   public static int solution(List<String> listX, List<String> listY) {
-	   int answer = 0;
-       for(int i = 0;i<listX.size();i++) {
-       	if(listY.contains(listX.get(i))) {
-       		answer = Math.max(answer, listX.get(i).length());
-       	}
-       }
-       return answer+1;
-   }
+	public static String ItoS(int n) {
+		return String.valueOf(n);
+	}
+	public static int StoI(String s) {
+		return Integer.parseInt(s);
+	}
 }
---
+++ src/CodingTest/PRO3_2023_11_04.java
@@ -0,0 +1,33 @@
+package CodingTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PRO3_2023_11_04 {
+	static String[] arr1 = {"A","A","B","C","D"};
+	static String[] arr2 = {"B","C","B","D"};
+	public static void main(String[] args) {
+        List<String> listX = new ArrayList<>();
+        List<String> listY = new ArrayList<>();
+        dic(arr1, 0, "", listX);
+        dic(arr2, 0, "", listY);
+        System.out.println(solution(listX, listY));
+	}
+   public static void dic(String[] arr, int index, String current, List<String> list) {
+        if (index == arr.length) {
+            list.add(current);
+            return;
+        }
+        dic(arr, index + 1, current + arr[index], list);
+        dic(arr, index + 1, current, list);
+    }
+   public static int solution(List<String> listX, List<String> listY) {
+	   int answer = 0;
+       for(int i = 0;i<listX.size();i++) {
+       	if(listY.contains(listX.get(i))) {
+       		answer = Math.max(answer, listX.get(i).length());
+       	}
+       }
+       return answer+1;
+   }
+}
